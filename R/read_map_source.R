e60cde187129c9f80d071a77e0fddef7731e2be1
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-02 11:57:26 -0800

    ✅ Pass test

--- R/read_map_source.R
+++ R/read_map_source.R
@@ -14,3 +14,9 @@ obtaine_number_of_points_of_each_routes <- function(waypoints) {
     as.numeric()
   return(n_points)
 }
+
+obtain_just_waypoints <- function(full_information) {
+  index <- obtain_index_of_route_rows(full_information)
+  last_index_of_waypoints <- index[1] - 2
+  return(full_information[last_index_of_waypoints, ])
+}
