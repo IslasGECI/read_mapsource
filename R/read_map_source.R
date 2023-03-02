0585d8128a4f708784637a1eada6668ec8cfbb01
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-02 11:19:37 -0800

    ✅ Pass test

--- R/read_map_source.R
+++ R/read_map_source.R
@@ -1,3 +1,7 @@
 read_ms <- function(path) {
   return(readr::read_tsv(path, skip = 3, show_col_types = FALSE))
 }
+
+obtain_index_of_route_rows <- function(waypoints) {
+  return(which(waypoints$Header == "Route"))
+}
