c96bb012fa933a4a194c66c4ea201f5b591357aa
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-02 11:07:52 -0800

    🛑🧪 Test `read_ms()`

---
+++ tests/testthat/test-read_map_source.R
@@ -0,0 +1,25 @@
+describe("Read a mapsource file", {
+  it("Read as tsv", {
+    read_ms("/workdir/tests/data/ig_traps.txt")
+  })
+})
+
+describe("Obtaine rows with Route", {
+  it("Number of lines", {
+    cameras <- read_ms("/workdir/tests/data/ig_cameras.txt")
+    rows_of_route <- obtain_index_of_route_rows(cameras)
+    expected <- 40
+    obtained <- length(rows_of_route)
+    expect_equal(obtained, expected)
+  })
+})
+
+describe("Obtaine number of points for each route", {
+  it("First four elements", {
+    cameras <- read_ms("/workdir/tests/data/ig_cameras.txt")
+    rows_of_route <- obtain_index_of_route_rows(cameras)
+    number_of_points <- obtaine_number_of_points_of_each_routes(cameras)
+    expected <- c(2,2,6,3)
+    expect_equal(number_of_points[1:4], expected)
+  })
+})
