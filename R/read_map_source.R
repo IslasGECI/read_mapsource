f0355bacb84184b01f881ebc9ad4cf3aa7685bfb
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-02 12:15:53 -0800

    🔧🚧 Add dependency

--- R/read_map_source.R
+++ R/read_map_source.R
@@ -1,3 +1,6 @@
+#' @importFrom magrittr %>%
+
+#' @export
 read_ms <- function(path) {
   return(readr::read_tsv(path, skip = 3, show_col_types = FALSE))
 }
@@ -15,6 +18,7 @@ obtaine_number_of_points_of_each_routes <- function(waypoints) {
   return(n_points)
 }
 
+#' @export
 obtain_just_waypoints <- function(full_information) {
   index <- obtain_index_of_route_rows(full_information)
   last_index_of_waypoints <- index[1] - 2
