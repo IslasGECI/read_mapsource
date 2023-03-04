f9effffa22513c3793a72cc23fb1105be1f25038
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-04 07:31:35 -0800

    🛑🧪 Test `obtain_csv_from_waypoints_of_mapsource()`
    There is not desactives traps

--- tests/testthat/test-obtain_csv_from_mapsource.R
+++ tests/testthat/test-obtain_csv_from_mapsource.R
@@ -4,11 +4,13 @@ describe("Obtain the csv of cameras from mapsource", {
   waypoints <- read_csv("/workdir/tests/data/example_waypoints_from_mapsource.csv", show_col_types = FALSE)
   obtained <- obtain_csv_from_waypoints_of_mapsource(waypoints)
   it("From waypoints example", {
-    expect_equal(obtained$Zona, rep("01", 10))
+    expect_equal(obtained$Zona, rep("01", 6))
   })
   it("Has the right positions", {
     expect_true(obtained$Coordenada_Este[1] == 375311)
     expect_true(obtained$Coordenada_Norte[1] == 3196733)
+    expect_true(obtained$Coordenada_Este[1] == 375236)
+    expect_true(obtained$Coordenada_Norte[1] == 3200753)
   })
   it("Has the right columns", {
     ig_cameras <- read_csv("/workdir/tests/data/IG_CAMARA_TRAMPA_EXTRA_26FEB2023.csv", show_col_types = FALSE)
