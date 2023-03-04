d45e1be1904a6b622c836c9212bbd0597084d58f
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-04 07:22:05 -0800

    ✅ Pass test

--- R/obtain_csv_from_mapsource.R
+++ R/obtain_csv_from_mapsource.R
@@ -1,7 +1,9 @@
 obtain_csv_from_waypoints_of_mapsource <- function(waypoints) {
   ig_cameras <- waypoints |>
     add_zone_column() |>
-    add_coordinates()
+    add_coordinates() |>
+    select_right_columns() |>
+    add_other_columns()
   return(ig_cameras)
 }
 
@@ -14,4 +16,22 @@ add_coordinates <- function(waypoints) {
     separate(Position, c(NA, NA, "Coordenada_Este", "Coordenada_Norte")) |>
     mutate(Coordenada_Este = as.numeric(Coordenada_Este)) |>
     mutate(Coordenada_Norte = as.numeric(Coordenada_Norte))
+}
+
+select_right_columns <- function(waypoints) {
+  modified <- waypoints |>
+    select(c(ID_camara=2,3,6,7))
+}
+
+add_other_columns <- function(waypoints) {
+  waypoints |>
+    add_column(
+      Fecha_revision = NA,
+      Responsable = NA,
+      Revision = NA,
+      Estado_camara = NA,
+      Estado_memoria = NA,
+      Porcentaje_bateria = NA,
+      Observaciones = NA
+    )
 }
