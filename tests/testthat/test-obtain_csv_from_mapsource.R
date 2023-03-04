5304df09fbcd621159f827d57a7b4c7b0984744c
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-04 07:35:11 -0800

    🪲✅ Fix test: right index

--- tests/testthat/test-obtain_csv_from_mapsource.R
+++ tests/testthat/test-obtain_csv_from_mapsource.R
@@ -9,8 +9,8 @@ describe("Obtain the csv of cameras from mapsource", {
   it("Has the right positions", {
     expect_true(obtained$Coordenada_Este[1] == 375311)
     expect_true(obtained$Coordenada_Norte[1] == 3196733)
-    expect_true(obtained$Coordenada_Este[1] == 375236)
-    expect_true(obtained$Coordenada_Norte[1] == 3200753)
+    expect_true(obtained$Coordenada_Este[6] == 375236)
+    expect_true(obtained$Coordenada_Norte[6] == 3200753)
   })
   it("Has the right columns", {
     ig_cameras <- read_csv("/workdir/tests/data/IG_CAMARA_TRAMPA_EXTRA_26FEB2023.csv", show_col_types = FALSE)
