b5e4981ff36bf36c8babdf68d0b90af625b064a8
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-03 19:38:35 -0800

    ✅ Pass test

--- R/obtain_csv_from_mapsource.R
+++ R/obtain_csv_from_mapsource.R
@@ -1,1 +1,9 @@
-obtain_csv_from_waypoints_of_mapsource <- function(waypoints) {}
+obtain_csv_from_waypoints_of_mapsource <- function(waypoints) {
+  ig_cameras <- waypoints |>
+    add_zone_column()
+  return(ig_cameras)
+}
+
+add_zone_column <- function(waypoints) {
+  modified <- waypoints |> separate(Name, c(NA, "Zona", NA, NA), remove = FALSE)
+}
--- tests/testthat/test-obtain_csv_from_mapsource.R
+++ tests/testthat/test-obtain_csv_from_mapsource.R
@@ -1,8 +1,12 @@
 library(tidyverse)
 
 describe("Obtain the csv of cameras from mapsource", {
+  waypoints <- read_csv("/workdir/tests/data/example_waypoints_from_mapsource.csv", show_col_types = FALSE)
+  obtained <- obtain_csv_from_waypoints_of_mapsource(waypoints)
   it("From waypoints example", {
-    waypoints <- read_csv("/workdir/tests/data/example_waypoints_from_mapsource.csv", show_col_types = FALSE)
-    obtained <- obtain_csv_from_waypoints_of_mapsource(waypoints)
+    expect_equal(obtained$Zona, rep("01", 10))
+  })
+  it("Has the right positions", {
+    expect_true(obtained$Coordenada_Este[1] == 375311)
   })
 })
