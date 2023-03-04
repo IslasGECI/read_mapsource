169a0470ac576e3a91826f2caadf75e09c37ee28
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-03 19:42:16 -0800

    ✅ Pass test

--- R/obtain_csv_from_mapsource.R
+++ R/obtain_csv_from_mapsource.R
@@ -1,9 +1,16 @@
 obtain_csv_from_waypoints_of_mapsource <- function(waypoints) {
   ig_cameras <- waypoints |>
-    add_zone_column()
+    add_zone_column() |>
+    add_east_coordinate()
   return(ig_cameras)
 }
 
 add_zone_column <- function(waypoints) {
   modified <- waypoints |> separate(Name, c(NA, "Zona", NA, NA), remove = FALSE)
+}
+
+add_east_coordinate <- function(waypoints) {
+  modified <- waypoints |> 
+    separate(Position, c(NA, NA, "Coordenada_Este", NA)) |>
+    mutate(Coordenada_Este = as.numeric(Coordenada_Este))
 }
