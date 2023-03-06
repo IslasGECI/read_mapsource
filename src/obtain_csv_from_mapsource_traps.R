be0346ba11bd573690c3a01d7efad73f397ccacc
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-06 10:34:28 -0800

    🤫 Mute message

--- src/obtain_csv_from_mapsource_traps.R
+++ src/obtain_csv_from_mapsource_traps.R
@@ -9,5 +9,5 @@ next_sunday <- obtain_date_to_title(today)
 output_file <- glue::glue(OUTPUT_MAPSOURCE_PATHS[[type_of_traps]])
 obtain_csv_from_traps_of_mapsource(cameras_path, today) |>
   select(-12) |>
-  left_join(traps_with_routes) |>
+  left_join(traps_with_routes, by = "ID") |>
   write_csv(output_file)
