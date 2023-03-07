d9d760ddd409d72d00361fdb90eae4958dc3f880
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-07 10:55:54 -0800

    🛑🧪 Test `MES_2_NUMBER_MONTH`

--- tests/testthat/test-file_for_map_of_traps.R
+++ tests/testthat/test-file_for_map_of_traps.R
@@ -4,9 +4,14 @@ describe("from_ig_position_2_maps_of_traps", {
 })
 
 describe("obtain_date_of_name_file", {
-  it(" returns '12-03-2023' from 'IG_POSICION_TRAMPA_12MAR2023.csv'", {
+  it("returns '12-03-2023' from 'IG_POSICION_TRAMPA_12MAR2023.csv'", {
     files_ig_pt <- "IG_POSICION_TRAMPA_12MAR2023.csv"
     obtained <- obtain_date_of_name_file(files_ig_pt)
     expect_equal(obtained, "12-03-2023")
+  })
+  it("returns '03' from 'MAR'", {
+    expected <- "03"
+    obtained <- MES_2_NUMBER_MONTH[["MAR"]]
+    expect_equal(obtained, expected)
   })
 })
