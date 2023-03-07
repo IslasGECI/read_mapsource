f8a5f1ea36dbbdfc31a7f87e13148beae3e072ab
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-07 12:01:38 -0800

    ✅ Pass test

--- R/file_for_map_of_traps.R
+++ R/file_for_map_of_traps.R
@@ -23,11 +23,12 @@ MES_2_NUMBER_MONTH <- list(
   "DIC" = "12"
 )
 
-transform_from_ig_position_2_maps_of_traps <- function(posicion_trampa) {
+transform_from_ig_position_2_maps_of_traps <- function(posicion_trampa, revised_date) {
   posicion_trampa |>
     filter_na_from_Nombre_del_responsable() |>
     select_first_columns() |>
-    add_is_active_column()
+    add_is_active_column() |>
+    add_revised_date(revised_date)
 }
 
 filter_na_from_Nombre_del_responsable <- function(posicion_trampa) {
@@ -43,4 +44,9 @@ select_first_columns <- function(posicion_trampa) {
 add_is_active_column <- function(posicion_trampa) {
   posicion_trampa |>
     add_column("is_active" = TRUE)
+}
+
+add_revised_date <- function(posicion_trampa, revised_date) {
+  posicion_trampa |>
+    add_column("date" = revised_date)
 }
