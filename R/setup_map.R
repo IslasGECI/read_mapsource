1600c954d7b85ade2d2aee841b14bcdb616995fd
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-10 08:32:28 -0800

    😇 Pass test

--- R/setup_map.R
+++ R/setup_map.R
@@ -4,3 +4,14 @@ remove_duplicated_id <- function(activated_or_inactivated_traps) {
     duplicated()
   return(activated_or_inactivated_traps[!is_duplicated_id, ])
 }
+
+obtain_activated_traps_from_ig_posicion_path <- function(ig_posicion_path) {
+  date_of_name_file <- "10-03-2023"
+  activated_traps <- read_csv(
+    ig_posicion_path,
+    show_col_types = FALSE,
+    col_types = "ciicccccccccc"
+  ) |>
+    transform_from_ig_position_2_maps_of_traps(date_of_name_file)
+  return(activated_traps)
+}
