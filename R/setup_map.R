848058f74f3d08f184e1a802d4b11441b9810aaa
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-10 07:34:27 -0800

    ✅ Pass test

---
+++ R/setup_map.R
@@ -0,0 +1,5 @@
+remove_duplicated_id <- function(activated_or_inactivated_traps) {
+  is_duplicated_id <- activated_or_inactivated_traps$ID |>
+    duplicated()
+  return(activated_or_inactivated_traps[!is_duplicated_id, ])
+}
