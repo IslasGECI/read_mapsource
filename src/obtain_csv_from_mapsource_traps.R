ef12132a152620c4c3e79d90f102fecad67b11b5
Mario Villasante <mario.villasante@islas.org.mx>
2023-04-21 15:04:04 -0700

    💉 Inyect function

--- src/obtain_csv_from_mapsource_traps.R
+++ src/obtain_csv_from_mapsource_traps.R
@@ -1,13 +1,7 @@
 library(tidyverse)
 library(lubridate)
 library(readMS)
-today <- ymd("2023-03-04")
 type_of_traps <- "cepos"
-cameras_path <- read_ms(MAPSOURCE_PATHS[[type_of_traps]])
-traps_with_routes <- obtain_traps_with_routes(cameras_path)
-next_sunday <- obtain_date_to_title(today)
-output_file <- glue::glue(OUTPUT_MAPSOURCE_PATHS[[type_of_traps]])
-obtain_csv_from_traps_of_mapsource(cameras_path, today) |>
-  select(-12) |>
-  left_join(traps_with_routes, by = "ID") |>
-  write_csv(output_file)
+mapsource_path <- "data/IG_TRAMPAS_16ABR2023.txt"
+
+write_position_tramps_csv(mapsource_path, type_of_traps)
