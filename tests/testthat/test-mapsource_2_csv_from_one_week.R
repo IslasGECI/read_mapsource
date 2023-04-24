c908609e192b974cc17c434d87d6cd4fe8b90200
Mario Villasante <mario.villasante@islas.org.mx>
2023-04-24 13:05:12 -0700

    🔥🖨️ Remove prints

--- tests/testthat/test-mapsource_2_csv_from_one_week.R
+++ tests/testthat/test-mapsource_2_csv_from_one_week.R
@@ -6,9 +6,9 @@ describe("Create csv from mapsource", {
     today <- ymd("2023-04-21")
     write_position_traps_for_one_week(mapsource_path, today = today)
     output_file <- "/workdir/data/IG_POSICION_TRAMPAS_23ABR2023.csv"
-    obtained_csv <- read_csv(output_file, show_col_types=FALSE)
+    obtained_csv <- read_csv(output_file, show_col_types = FALSE)
     expected_path <- "../data/expected_IG_POSICION_TRAMPAS_23ABR2023.csv"
-    expected_csv <- read_csv(expected_path, show_col_types=FALSE)
+    expected_csv <- read_csv(expected_path, show_col_types = FALSE)
     expect_equal(obtained_csv, expected_csv)
     delete_output_file(output_file)
   })
