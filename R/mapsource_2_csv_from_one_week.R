297451e6810e253c2bddcf782ab284121609f42b
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-05-04 12:17:33 -0700

    ♻️Extract function 'add_traps_status()'

--- R/mapsource_2_csv_from_one_week.R
+++ R/mapsource_2_csv_from_one_week.R
@@ -1,13 +1,17 @@
 copy_trap_status <- function(traps_without_status, traps_status) {
   aux <- join_traps_status(traps_without_status, traps_status)
-  aux[,4:10] <- aux[,14]
-  traps_with_status <- aux |>
-    select(1:3,13,4:12)
+  traps_with_status <- add_traps_status(aux)
   return(traps_with_status)
 }
 
 join_traps_status <- function(traps_without_status, traps_status) {
   traps_without_status |>
     select(-Nombre_del_responsable) |>
-    left_join(traps_status, by="ID")
+    left_join(traps_status, by = "ID")
+}
+
+add_traps_status <- function(aux) {
+  aux[, 4:10] <- aux[, 14]
+  aux |>
+    select(1:3, 13, 4:12)
 }
