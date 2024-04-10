4ad4d842af47ec28636962da58acfa71b7b81a03
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-04-10 10:22:48 -0700

    ✅ Pass tests

--- tests/testthat/test-check_cameras_in_mapsource.R
+++ tests/testthat/test-check_cameras_in_mapsource.R
@@ -1,7 +1,7 @@
 describe("Check cameras", {
   listed_cameras_in_mapsource <- tibble("Name" = c("CT-03-034-AR", "CT-01-001-CF", "CT-07-011-LM"))
   cameras_in_revision_campo <- tibble("ID_camara" = c("CT-01-001-CF", "CT-99-100-XX"))
-  cameras_in_revision_campo <- tibble("ID_camara" = c("CT-01-001-CF", "CT-03-034-AR", "CT-07-011-LM"))
+  cameras_in_revision_campo_green <- tibble("ID_camara" = c("CT-01-001-CF", "CT-03-034-AR", "CT-07-011-LM"))
   it("check if cameras in revision_campo are in mapsource", {
     obtained <- are_all_in_mapsource(cameras_in_revision_campo, listed_cameras_in_mapsource)
     expect_false(obtained)
