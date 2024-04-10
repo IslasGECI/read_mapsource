92346e5c5c3f2bf943be5a0e8c0041e1d642b4dd
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-04-10 11:47:43 -0700

    ✅ Pass tests

--- R/check_cameras_in_mapsource.R
+++ R/check_cameras_in_mapsource.R
@@ -7,6 +7,21 @@ check_cameras_in_mapsource <- function(cameras_in_revision_campo, cameras_in_map
   stop(messages)
 }
 
+check_cameras_in_revision_campo <- function(cameras_in_revision_campo, cameras_in_mapsource) {
+  if (are_id_sets_equal(cameras_in_revision_campo, cameras_in_mapsource)) {
+    message("💚 Todas las cámaras están en el mapsource 💚")
+    return()
+  }
+  messages <- .write_missing_id_in_revision_campo_message(cameras_in_revision_campo, cameras_in_mapsource)
+  stop(messages)
+}
+
+.write_missing_id_in_revision_campo_message <- function(cameras_in_revision_campo, cameras_in_mapsource) {
+  missing_ids <- get_missing_ids_in_revision_campo(cameras_in_revision_campo, cameras_in_mapsource)
+  different_ids <- glue::glue_collapse(missing_ids, ", ", last = " y ")
+  glue::glue("🚨 Los IDs {different_ids} en mapsource no están en el revision_campo 🚨")
+}
+
 .write_missing_id_in_mapsource_message <- function(cameras_in_revision_campo, cameras_in_mapsource) {
   missing_ids <- get_missing_ids_in_mapsource(cameras_in_revision_campo, cameras_in_mapsource)
   different_ids <- glue::glue_collapse(missing_ids, ", ", last = " y ")
