6618bfeab4100e024747aec1641e585e6c283bf4
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-04-10 11:44:07 -0700

    🛑🧪 Fail tests

--- tests/testthat/test-check_cameras_in_mapsource.R
+++ tests/testthat/test-check_cameras_in_mapsource.R
@@ -26,6 +26,6 @@ describe("Check cameras", {
     expect_error(check_cameras_in_mapsource(cameras_in_revision_campo, listed_cameras_in_mapsource), "🚨 Los IDs CT-99-100-XX en IG_CAMARAS no están en el mapsource 🚨")
   })
   it("Get error if cameras in mapsource are not in revision_campo", {
-    expect_error(check_cameras_in_mapsource(two_cameras_in_revision_campo, listed_cameras_in_mapsource), "🚨 Los IDs CT-01-001-CF en mapsource no están en el revision_campo 🚨")
+    expect_error(check_cameras_in_revision_campo(two_cameras_in_revision_campo, listed_cameras_in_mapsource), "🚨 Los IDs CT-01-001-CF en mapsource no están en el revision_campo 🚨")
   })
 })
