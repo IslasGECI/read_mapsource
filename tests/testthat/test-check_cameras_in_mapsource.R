f23aac5ed56b543c0100d0f44a9f292331a58c29
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-04-10 09:59:27 -0700

    🛑🧪 Fail tests

--- tests/testthat/test-check_cameras_in_mapsource.R
+++ tests/testthat/test-check_cameras_in_mapsource.R
@@ -1,9 +1,9 @@
 describe("Check cameras", {
-  listed_cameras <- read_csv("/workdir/tests/data/ig_cameras_waypoints.csv", show_col_types = FALSE)
-  rows_of_installed_cameras <- extract_installed_cameras(listed_cameras)
-  write_csv(rows_of_installed_cameras, "/workdir/tests/data/installed_cameras.csv")
+  listed_cameras_in_mapsource <- tibble("Name" = c("CT-03-034-AR", "CT-01-001-CF", "CT-07-011-LM"))
   cameras_in_revision_campo <- tibble("ID_camara" = c("CT-01-001-CF", "CT-99-100-XX"))
   it("check if cameras in revision_campo are in mapsource", {
-    obtained <- check_cameras_in_mapsource(cameras_in_revision_campo, rows_of_installed_cameras)
+    obtained <- are_all_in_mapsource(cameras_in_revision_campo, listed_cameras_in_mapsource)
+    expect_false(obtained)
+    # expect_error(check_cameras_in_mapsource(cameras_in_revision_campo, listed_cameras_in_mapsource), "🚨 Los IDs CT-99-100-XX en IG_CAMARAS no están en el mapsource 🚨")
   })
 })
