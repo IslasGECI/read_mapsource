c3eabf3792b868014b13adf5c9696d92e4facc1b
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-04-10 10:55:12 -0700

    ✅ Pass tests

--- R/check_cameras_in_mapsource.R
+++ R/check_cameras_in_mapsource.R
@@ -3,7 +3,8 @@ check_cameras_in_mapsource <- function(cameras_in_revision_campo, cameras_in_map
     message("💚 Todas las cámaras están en el mapsource 💚")
     return()
   }
-  stop("🚨 Error en los IDs 🚨")
+  missing_ids <- get_missing_ids_in_mapsource(cameras_in_revision_campo, cameras_in_mapsource)
+  stop("🚨 Los IDs CT-99-100-XX en IG_CAMARAS no están en el mapsource 🚨")
 }
 
 get_missing_ids_in_mapsource <- function(cameras_in_revision_campo, listed_cameras_in_mapsource) {
