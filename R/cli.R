498bc203604e136af2a1e0dd5ed80f79b98721c9
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-04-10 14:16:36 -0700

    ♻️  Refactor

--- R/cli.R
+++ R/cli.R
@@ -1,3 +1,4 @@
+#' @export
 check_cameras <- function(revision_campo_path, mapsource_path) {
   mapsource_cameras <- read_listed_cameras_from_mapsource(mapsource_path)
   revision_campo <- readr::read_csv(revision_campo_path, show_col_types = FALSE)
