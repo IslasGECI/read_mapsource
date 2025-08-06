1c210dc32a58e5bde3dc53e8dcc34976e128a8ac
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-08-06 14:58:35 -0700

    ♻️  Refactor

--- R/check_cameras_in_mapsource.R
+++ R/check_cameras_in_mapsource.R
@@ -7,7 +7,7 @@ double_check <- function(cameras_in_revision_campo, cameras_in_mapsource) {
 check_cameras_in_mapsource <- function(cameras_in_revision_campo, cameras_in_mapsource) {
   missing_ids <- get_missing_ids_in_mapsource(cameras_in_revision_campo[["ID_camara"]], cameras_in_mapsource[["Name"]])
   if (length(missing_ids) > 0) {
-    messages <- .write_missing_id_in_mapsource_message(cameras_in_revision_campo, cameras_in_mapsource)
+    messages <- .write_missing_id_in_mapsource_message(missing_ids)
     stop(messages)
   }
 }
@@ -26,8 +26,7 @@ check_cameras_in_revision_campo <- function(cameras_in_revision_campo, cameras_i
   glue::glue("🚨 Los IDs {different_ids} en mapsource no están en el revision_campo 🚨")
 }
 
-.write_missing_id_in_mapsource_message <- function(cameras_in_revision_campo, cameras_in_mapsource) {
-  missing_ids <- get_missing_ids_in_mapsource(cameras_in_revision_campo[["ID_camara"]], cameras_in_mapsource[["Name"]])
+.write_missing_id_in_mapsource_message <- function(missing_ids) {
   different_ids <- glue::glue_collapse(missing_ids, ", ", last = " y ")
   glue::glue("🚨 Los IDs {different_ids} en IG_CAMARAS no están en el mapsource 🚨")
 }
