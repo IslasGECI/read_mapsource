42734d9aa1e8a44634a5c57bd4e8ce65a5374a62
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-08-14 10:46:22 -0700

    ♻️  Refactor

--- R/check_cameras_in_mapsource.R
+++ R/check_cameras_in_mapsource.R
@@ -5,7 +5,7 @@ double_check <- function(cameras_in_revision_campo, cameras_in_mapsource) {
 }
 
 check_cameras_in_mapsource <- function(cameras_in_revision_campo, cameras_in_mapsource) {
-  missing_ids <- get_missing_ids_in_mapsource(cameras_in_revision_campo[["ID_camara"]], cameras_in_mapsource[["Name"]])
+  missing_ids <- get_missing_ids(cameras_in_revision_campo[["ID_camara"]], cameras_in_mapsource[["Name"]])
   if (length(missing_ids) > 0) {
     messages <- .write_missing_id_in_mapsource_message(missing_ids)
     stop(messages)
@@ -30,7 +30,7 @@ check_cameras_in_revision_campo <- function(cameras_in_revision_campo, cameras_i
   glue::glue("🚨 Los IDs {different_ids} en IG_CAMARAS no están en el mapsource 🚨")
 }
 
-get_missing_ids_in_mapsource <- function(cameras_ids, mapsource_ids) {
+get_missing_ids <- function(cameras_ids, mapsource_ids) {
   setdiff(cameras_ids, mapsource_ids)
 }
 
--- R/file_for_map_of_traps.R
+++ R/file_for_map_of_traps.R
@@ -54,7 +54,7 @@ update_activated_traps <- function(inactive_traps, activated_traps) {
 }
 
 check_traps_in_mapsource <- function(activated_traps, inactive_traps) {
-  missing_ids <- get_missing_ids_in_mapsource(activated_traps$ID, inactive_traps$ID)
+  missing_ids <- get_missing_ids(activated_traps$ID, inactive_traps$ID)
   if (length(missing_ids) > 0) {
     different_ids <- glue::glue_collapse(missing_ids, ", ", last = " y ")
     stop(glue::glue("🚨 Los IDs {different_ids} en IG_POSICION no están en el mapsource 🚨"))
