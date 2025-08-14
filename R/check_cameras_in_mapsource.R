5aba4a82f26b9d903509355ed49e00074558eb72
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-08-14 10:47:41 -0700

    ♻️  Refactor

--- R/check_cameras_in_mapsource.R
+++ R/check_cameras_in_mapsource.R
@@ -13,7 +13,7 @@ check_cameras_in_mapsource <- function(cameras_in_revision_campo, cameras_in_map
 }
 
 check_cameras_in_revision_campo <- function(cameras_in_revision_campo, cameras_in_mapsource) {
-  missing_ids <- get_missing_ids_in_revision_campo(cameras_in_revision_campo, cameras_in_mapsource)
+  missing_ids <- get_missing_ids(cameras_in_mapsource[["Name"]], cameras_in_revision_campo[["ID_camara"]])
   if (length(missing_ids) > 0) {
     messages <- .write_missing_id_in_revision_campo_message(missing_ids)
     stop(messages)
