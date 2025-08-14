890c1b5f9f075178bd12d6a3dfa01a29ebb89dab
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-08-14 13:28:46 -0700

    ✅ Pass tests

--- R/cli.R
+++ R/cli.R
@@ -5,3 +5,9 @@ check_cameras <- function(revision_campo_path, mapsource_path) {
   installed_cameras <- extract_installed_cameras(mapsource_cameras)
   double_check(revision_campo, installed_cameras)
 }
+#' @export
+check_traps <- function(position_path, mapsource_path) {
+  mapsource_traps <- read_listed_cameras_from_mapsource(mapsource_path) |> mutate(ID = Name)
+  position_traps <- readxl::read_xlsx(position_path)
+  double_check_traps(mapsource_traps, position_traps)
+}
